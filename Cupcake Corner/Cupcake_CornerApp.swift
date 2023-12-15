a4a084ded5432b4621a5c48faecb04c1159c593f
joy jain <joyshah204@gmial.com>
2023-12-15 23:48:08 +0530

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by JOY JAIN on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
